2af2a4e9599e690a14b83f70fbfe6100edafe81f
Vovan_Super <ovsyukov@yandex.com>
2020-04-10 18:31:06 +0300

    (WIP):add husky prepush

---
+++ scripts/hooks/prepush.js
@@ -0,0 +1,46 @@
+const { commitPrettier } = require('./prettier');
+const { resolve } = require('path');
+const simpleGit = require('simple-git/promise');
+const chalk = require('chalk');
+
+// Computed Deps
+const root = resolve(__dirname, '../..');
+const git = simpleGit(root);
+
+const notCleanTreeString = chalk`
+{red Cannot push non clean tree (stash or add/commit all )}
+
+`;
+
+(async () => {
+  try {
+    const hasDiff = !!(await git.diff());
+
+    if (hasDiff) {
+      console.error(notCleanTreeString);
+      return process.exit(1);
+    }
+
+    const diff = await git.diff([
+      '--name-only',
+      '--diff-filter=d',
+      'origin/master...HEAD'
+    ]);
+    const changedFiles = diff.split('\n');
+
+    await commitPrettier(changedFiles);
+
+    console.log(chalk`
+Pre-Push Validation Succeeded
+
+`);
+    process.exit();
+  } catch (err) {
+    console.error(chalk`
+{red Pre-Push Validation Failed, error body below}
+
+`);
+    console.error(err);
+    return process.exit(1);
+  }
+})();
---
+++ scripts/hooks/push.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+npm run-script build:sample
+git add .
+git commit -am ''
