8ebb10634ee53adb7bbb2247d3270dc85c684870
Vovan_Super <ovsyukov@yandex.com>
2020-04-16 11:07:56 +0300

    release v0.1.49

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env sh
 
 ROOT=$(pwd)
-sh $ROOT/scripts/versioning/pack-version.sh
+PACK_VER=$ROOT/scripts/versioning/pack-version.sh
+NODE_JQ=$ROOT/node_modules/node-jq/bin/jq
+PUSH_TAGGED=$ROOT/scripts/push-tagged.sh
+
+chmod +x $PACK_VER $NODE_JQ $PUSH_TAGGED  
+sh $PACK_VER
 
 verStr=".version"
-TAGSTR=$($ROOT/node_modules/node-jq/bin/jq $verStr package.json)
+TAGSTR=$($NODE_JQ $verStr package.json)
 
 temp="${TAGSTR%\"}"
 temp="${temp#\"}"
 
 TAGPREF="v"
 TAG=$TAGPREF"$temp"
-echo "$TAG" | sh $ROOT/scripts/push-tagged.sh
+echo "$TAG" | sh $PUSH_TAGGED
